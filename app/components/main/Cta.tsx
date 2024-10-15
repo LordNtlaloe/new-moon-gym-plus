5ccf144e1bc7e2267870dc746fe326812485c0b8
LordNtlaloe <ntlal0e182@gmail.com>
2024-10-15 18:58:19 +0200

    Bug: Fixed Call To Action Issue

--- app/components/main/Cta.tsx
+++ app/components/main/Cta.tsx
@@ -6,12 +6,9 @@ function Cta() {
     <section id="cta">
       <div className="bg-[url('/images/cta/bg.jpg')] px-8 py-16 bg-[50%] bg-no-repeat bg-cover mt-8">
         <div className="text-white flex flex-col gap-4 font-bold max-w-screen-xl m-auto">
-          <h1 className="text-4xl">Need a Fitness Trainer?</h1>
+          <h1 className="text-4xl">Want To Book A Consultation?</h1>
           <h3 className="text-2xl flex items-center gap-2">
-            <span className="text-[#ff0000]">Call:</span>
-            <span>+123-456789</span>
-          </h3>
-          <div>
+            <span className="text-[#ff0000]">Click Here:</span>
             <Link
               href="/"
               className="relative text-white flex items-center gap-2 bg-[#ff0000] font-bold px-6 py-4 after:h-[3.5rem] after:absolute after:w-[13.25rem] after:duration-300 after:ease-linear after:border after:border-[#ff0000] after:-top-3 after:-right-3 hover:after:top-0 hover:after:right-0 mt-6 w-fit"
@@ -21,6 +18,9 @@ function Cta() {
                 <i className="fa-solid fa-arrow-right undefined"></i>
               </span>
             </Link>
+          </h3>
+          <div>
+            
           </div>
         </div>
       </div>
