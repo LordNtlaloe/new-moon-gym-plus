fb2abf3dd263808a2a104818fbc005823d2a91c0
LordNtlaloe <ntlal0e182@gmail.com>
2024-10-20 00:46:03 +0200

    Feature: Added Guest Login

---
+++ app/(pages)/online-sessions/[id]/SessionLoginPage.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { ClerkLoaded, ClerkLoading, SignInButton } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function SessionLoginPage() {
+    return (
+        <div className='mx-auto w-fit space-y-3'>
+            <h1 className="text-center text-2xl font-bold">Join Session</h1>
+            <ClerkLoaded>
+                <SignInButton>
+                    <Button className='w-44'>Sign In</Button>
+                </SignInButton>
+                <Link href="?guest=true" className='w-44 bg-gray-500'>Join As Guest</Link>
+            </ClerkLoaded>
+            <ClerkLoading>
+                <Loader2 className='animate-spin mx-auto text-2xl'/>
+            </ClerkLoading>
+        </div>
+    )
+}
--- app/(pages)/online-sessions/[id]/page.tsx
+++ app/(pages)/online-sessions/[id]/page.tsx
@@ -2,21 +2,31 @@ import Banner from '@/components/main/Banner';
 import { Metadata } from 'next';
 import React from 'react'
 import SessionPage from './SessionPage';
+import { currentUser } from '@clerk/nextjs/server';
+import SessionLoginPage from './SessionLoginPage';
 
-interface PageProps{
-    params: {id: string}
+interface PageProps {
+  params: { id: string };
+  searchParams: {guest: string}
 }
 
-export function generateMetadata({params: {id}}:PageProps ): Metadata{
-    return{
-        title: `Session: ${id}`
-    }
+export function generateMetadata({ params: { id } }: PageProps): Metadata {
+  return {
+    title: `Session: ${id}`
+  }
 }
 
-export default function page({params: {id}}: PageProps) {
+export default async function page({ params: { id }, searchParams: {guest}}: PageProps) {
+  const user = await currentUser();
+  const guestMode = guest === "true";
+  if(!user && !guestMode){
+    return(
+      <SessionLoginPage />
+    )
+  }
   return (
     <div className='my-40'>
-        <SessionPage id={id}/>
+      <SessionPage id={id} />
     </div>
   )
 }
