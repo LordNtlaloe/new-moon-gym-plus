56e0dcd91617ae77f7070c60b07c63d5ba279bbb
Inspirion <inspirion@192>
2025-03-19 17:31:54 +0200

    Updated Feature On Session List

--- components/SessionTypeList.tsx
+++ components/SessionTypeList.tsx
@@ -77,91 +77,93 @@ const SessionTypeList = () => {
   const allowedActions = userRole === 'Admin' || userRole === 'Trainer';
 
   return (
-    <ScrollArea className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      {allowedActions && (
-        <>
-          <HomeCard
-            img="/icons/add-session.svg"
-            title="New "
-            description="Start an instant Session"
-            handleClick={() => setSessionState('isInstantSession')}
-          />
-          <HomeCard
-            img="/icons/schedule.svg"
-            title="Schedule Session"
-            description="Plan your Session"
-            className="bg-purple-1"
-            handleClick={() => setSessionState('isScheduleSession')}
-          />
-        </>
-      )}
-      <HomeCard
-        img="/icons/join-session.svg"
-        title="Join Session"
-        description="via invitation link"
-        className="bg-blue-1"
-        handleClick={() => setSessionState('isJoiningSession')}
-      />
-      <HomeCard
-        img="/icons/recordings.svg"
-        title="View Recordings"
-        description="Session Recordings"
-        className="bg-yellow-1"
-        handleClick={() => router.push('/online-sessions/recordings')}
-      />
+    <ScrollArea>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        {allowedActions && (
+          <>
+            <HomeCard
+              img="/icons/add-session.svg"
+              title="New "
+              description="Start an instant Session"
+              handleClick={() => setSessionState('isInstantSession')}
+            />
+            <HomeCard
+              img="/icons/schedule.svg"
+              title="Schedule Session"
+              description="Plan your Session"
+              className="bg-purple-1"
+              handleClick={() => setSessionState('isScheduleSession')}
+            />
+          </>
+        )}
+        <HomeCard
+          img="/icons/join-session.svg"
+          title="Join Session"
+          description="via invitation link"
+          className="bg-blue-1"
+          handleClick={() => setSessionState('isJoiningSession')}
+        />
+        <HomeCard
+          img="/icons/recordings.svg"
+          title="View Recordings"
+          description="Session Recordings"
+          className="bg-yellow-1"
+          handleClick={() => router.push('/online-sessions/recordings')}
+        />
 
-      {SessionState === 'isScheduleSession' && allowedActions && (
+        {SessionState === 'isScheduleSession' && allowedActions && (
+          <SessionModal
+            isOpen
+            onClose={() => setSessionState(undefined)}
+            title="Create Session"
+            handleClick={createSession}
+          >
+            <div className="flex flex-col gap-2.5">
+              <label className="text-base font-normal text-sky-2">Add a description</label>
+              <Textarea
+                className="border-none bg-dark-3"
+                onChange={(e) => setValues({ ...values, description: e.target.value })}
+              />
+            </div>
+            <div className="flex w-full flex-col gap-2.5">
+              <label className="text-base font-normal text-sky-2">Select Date and Time</label>
+              <ReactDatePicker
+                selected={values.dateTime}
+                onChange={(date) => setValues({ ...values, dateTime: date! })}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+              />
+            </div>
+          </SessionModal>
+        )}
+
         <SessionModal
-          isOpen
+          isOpen={SessionState === 'isJoiningSession'}
           onClose={() => setSessionState(undefined)}
-          title="Create Session"
-          handleClick={createSession}
+          title="Type the link here"
+          buttonText="Join Session"
+          handleClick={() => router.push(values.link)}
         >
-          <div className="flex flex-col gap-2.5">
-            <label className="text-base font-normal text-sky-2">Add a description</label>
-            <Textarea
-              className="border-none bg-dark-3"
-              onChange={(e) => setValues({ ...values, description: e.target.value })}
-            />
-          </div>
-          <div className="flex w-full flex-col gap-2.5">
-            <label className="text-base font-normal text-sky-2">Select Date and Time</label>
-            <ReactDatePicker
-              selected={values.dateTime}
-              onChange={(date) => setValues({ ...values, dateTime: date! })}
-              showTimeSelect
-              timeFormat="HH:mm"
-              timeIntervals={15}
-              dateFormat="MMMM d, yyyy h:mm aa"
-              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
-            />
-          </div>
+          <Input
+            placeholder="Session link"
+            onChange={(e) => setValues({ ...values, link: e.target.value })}
+            className="border-none bg-dark-3"
+          />
         </SessionModal>
-      )}
 
-      <SessionModal
-        isOpen={SessionState === 'isJoiningSession'}
-        onClose={() => setSessionState(undefined)}
-        title="Type the link here"
-        buttonText="Join Session"
-        handleClick={() => router.push(values.link)}
-      >
-        <Input
-          placeholder="Session link"
-          onChange={(e) => setValues({ ...values, link: e.target.value })}
-          className="border-none bg-dark-3"
-        />
-      </SessionModal>
-
-      {allowedActions && (
-        <SessionModal
-          isOpen={SessionState === 'isInstantSession'}
-          onClose={() => setSessionState(undefined)}
-          title="Start an Instant Session"
-          buttonText="Start Session"
-          handleClick={createSession}
-        />
-      )}
+        {allowedActions && (
+          <SessionModal
+            isOpen={SessionState === 'isInstantSession'}
+            onClose={() => setSessionState(undefined)}
+            title="Start an Instant Session"
+            buttonText="Start Session"
+            handleClick={createSession}
+          />
+        )}
+      </div>
     </ScrollArea>
   );
 };
