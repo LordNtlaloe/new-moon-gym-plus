73cf929dafa1bacd29192f7cb245c9163ff21a25
Inspirion <inspirion@192>
2025-03-20 16:53:20 +0200

    Fixed Layout

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -10,7 +10,7 @@ import {
   useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Users, LayoutList } from 'lucide-react';
+import { Users, LayoutList, BetweenHorizonalEnd, BetweenVerticalEnd, Grid } from 'lucide-react';
 
 import {
   DropdownMenu,
@@ -23,37 +23,34 @@ import Loader from './Loader';
 import EndCallButton from './EndCallButton';
 import { cn } from '@/lib/utils';
 
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
+type CallLayoutType = 'speaker-vert' | 'speaker-horiz' | 'grid';
 
 const SessionRoom = () => {
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get('personal');
   const router = useRouter();
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-vert');
   const [showParticipants, setShowParticipants] = useState(false);
   const { useCallCallingState } = useCallStateHooks();
 
-  // for more detail about types of CallingState see: https://getstream.io/video/docs/react/ui-cookbook/ringing-call/#incoming-call-panel
   const callingState = useCallCallingState();
-
   if (callingState !== CallingState.JOINED) return <Loader />;
 
-  const CallLayout = () => {
-    switch (layout) {
-      case 'grid':
-        return <PaginatedGridLayout />;
-      case 'speaker-right':
-        return <SpeakerLayout participantsBarPosition="left" />;
-      default:
-        return <SpeakerLayout participantsBarPosition="right" />;
+  const CallLayoutView = () => {
+    if (layout === 'speaker-vert') {
+      return <SpeakerLayout />;
     }
+    if (layout === 'speaker-horiz') {
+      return <SpeakerLayout participantsBarPosition="right" />;
+    }
+    return <PaginatedGridLayout />;
   };
 
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
-        <div className=" flex size-full max-w-[1000px] items-center">
-          <CallLayout />
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayoutView />
         </div>
         <div
           className={cn('h-[calc(100vh-86px)] hidden ml-2', {
@@ -63,24 +60,18 @@ const SessionRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 mt-10">
         <CallControls onLeave={() => router.push(`/`)} />
-
         <DropdownMenu>
           <div className="flex items-center">
-            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
               <LayoutList size={20} className="text-white" />
             </DropdownMenuTrigger>
           </div>
           <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
-            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+            {['Grid', 'Speaker-Vert', 'Speaker-Horiz'].map((item, index) => (
               <div key={index}>
-                <DropdownMenuItem
-                  onClick={() =>
-                    setLayout(item.toLowerCase() as CallLayoutType)
-                  }
-                >
+                <DropdownMenuItem onClick={() => setLayout(item.toLowerCase() as CallLayoutType)}>
                   {item}
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="border-dark-1" />
@@ -90,7 +81,7 @@ const SessionRoom = () => {
         </DropdownMenu>
         <CallStatsButton />
         <button onClick={() => setShowParticipants((prev) => !prev)}>
-          <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
             <Users size={20} className="text-white" />
           </div>
         </button>
